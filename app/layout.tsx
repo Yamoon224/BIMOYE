68aa5a55b498751638b00b50883070af39ce0737
Yamoon224 <Yamooon664@gmail.com>
2025-09-05 00:37:22 +0000

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Footer } from "@/components/footer"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "BiMOYE - Prestation Multi-Services",
+  title: "BiMOYE",
   description:
     "Découvrez et réservez des résidences exceptionnelles pour vos vacances. Villas, chalets, appartements de luxe dans les plus belles destinations.",
   generator: "Yamoussa KEITA",
